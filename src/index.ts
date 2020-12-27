5fcb4ba13b517da9216b43c66e7860aa40c9ecf9
Bekki <moonbladez@hotmail.com>
2020-12-27 20:13:42 +0100

    server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello");
+//modules
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (request: Request, response: Response) => {
+  response.send(`
+        <div><h1>Hi</h1></div>
+    `);
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
